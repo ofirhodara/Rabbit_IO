b267a3910878423614bcec3b3ae2f4c050f95287
ofir hodara <ofir1682002@gmail.com>
2022-06-04 21:18:33 +0300

    kn and es works on compose now
    handle vm size problems with them, super fun to start working!

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
+import logging
+import ecs_logging
+
+
+logger = logging.getLogger("app")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler('logs/logs.json')
+handler.setFormatter(ecs_logging.StdlibFormatter())
+logger.addHandler(handler)
+
 if __name__ == '__main__':
-    for i in range(0, 10):
-        print("ofir")
+    for i in range(0, 5):
+        message = f"Write log number {i}"
+        print(message)
+        logger.debug(message +" - Hi world!")
